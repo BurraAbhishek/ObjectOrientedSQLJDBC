2e3cab473b5a74c8e48ca044deb17abea49c9432
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-07-24 23:21:58 +0530

    Declare class variables as private

--- src/stmt/Insert.java
+++ src/stmt/Insert.java
@@ -8,11 +8,11 @@ import java.util.*;
 import java.math.*;
 
 public class Insert {
-	public String table = "";
+	private String table = "";
 	private PreparedStatement stmt;
-	public ArrayList<String> attributes = new ArrayList<String>();
-	public ArrayList<String> datatypes = new ArrayList<String>();
-	public ArrayList<Object> values = new ArrayList<Object>();
+	private ArrayList<String> attributes = new ArrayList<String>();
+	private ArrayList<String> datatypes = new ArrayList<String>();
+	private ArrayList<Object> values = new ArrayList<Object>();
 	public void insert() throws Exception {
 		try {
 			if(new JDBCDriver().isDriverSupported()) {
@@ -20,61 +20,65 @@ public class Insert {
 				if(l != this.values.size() || l != this.datatypes.size()) {
 					throw new Exception("One or more elements is missing");
 				}
-				String s = "INSERT INTO " + this.table + " (";
-				for(int i = 0; i < l - 1; i++) {
-					s += this.attributes.get(i);
-					s += ", ";
-				}
-				Connection conn = new JDBConnection().generateConnection();
-				s += this.attributes.get(l - 1);
-				s += ") VALUES (";
-				for(int i = 0; i < l - 1; i++) {
-					s += "?, ";
-				}
-				s += "?)";
-				this.stmt = conn.prepareStatement(s);
-				for(int i = 0; i < l; i++) {
-					String type = this.datatypes.get(i);
-					switch(type) {
-					case "String":
-						this.stmt.setString(i+1, (String) this.values.get(i));
-						break;
-					case "NString":
-						this.stmt.setNString(i+1, (String) this.values.get(i));
-						break;
-					case "Integer":
-						this.stmt.setInt(i+1, (Integer) this.values.get(i));
-						break;
-					case "Double":
-						this.stmt.setDouble(i+1, (Double) this.values.get(i));
-						break;
-					case "Array":
-						this.stmt.setArray(i+1, (Array) this.values.get(i));
-						break;
-					case "Long":
-						this.stmt.setLong(i+1, (Long) this.values.get(i));
-						break;
-					case "BigDecimal":
-						this.stmt.setBigDecimal(i+1, (BigDecimal) this.values.get(i));
-						break;
-					case "Boolean":
-						this.stmt.setBoolean(i+1, (Boolean) this.values.get(i));
-						break;
-					case "Byte":
-						this.stmt.setByte(i+1, (Byte) this.values.get(i));
-						break;
-					case "Time":
-						this.stmt.setTime(i+1, (Time) this.values.get(i));
-						break;
-					case "Timestamp":
-						this.stmt.setTimestamp(i+1, (Timestamp) this.values.get(i));
-						break;
-					default:
-						this.stmt.setObject(i+1, this.values.get(i));
-						break;
+				Connection conn = new JDBConnection().generateDBConnection();
+				if(conn == null) {
+					throw new Exception("No connection to server");
+				} else {
+					String s = "INSERT INTO " + this.table + " (";
+					for(int i = 0; i < l - 1; i++) {
+						s += this.attributes.get(i);
+						s += ", ";
 					}
+					s += this.attributes.get(l - 1);
+					s += ") VALUES (";
+					for(int i = 0; i < l - 1; i++) {
+						s += "?, ";
+					}
+					s += "?)";
+					this.stmt = conn.prepareStatement(s);
+					for(int i = 0; i < l; i++) {
+						String type = this.datatypes.get(i);
+						switch(type) {
+						case "String":
+							this.stmt.setString(i+1, (String) this.values.get(i));
+							break;
+						case "NString":
+							this.stmt.setNString(i+1, (String) this.values.get(i));
+							break;
+						case "Integer":
+							this.stmt.setInt(i+1, (Integer) this.values.get(i));
+							break;
+						case "Double":
+							this.stmt.setDouble(i+1, (Double) this.values.get(i));
+							break;
+						case "Array":
+							this.stmt.setArray(i+1, (Array) this.values.get(i));
+							break;
+						case "Long":
+							this.stmt.setLong(i+1, (Long) this.values.get(i));
+							break;
+						case "BigDecimal":
+							this.stmt.setBigDecimal(i+1, (BigDecimal) this.values.get(i));
+							break;
+						case "Boolean":
+							this.stmt.setBoolean(i+1, (Boolean) this.values.get(i));
+							break;
+						case "Byte":
+							this.stmt.setByte(i+1, (Byte) this.values.get(i));
+							break;
+						case "Time":
+							this.stmt.setTime(i+1, (Time) this.values.get(i));
+							break;
+						case "Timestamp":
+							this.stmt.setTimestamp(i+1, (Timestamp) this.values.get(i));
+							break;
+						default:
+							this.stmt.setObject(i+1, this.values.get(i));
+							break;
+						}
+					}
+					this.stmt.execute();
 				}
-				this.stmt.execute();
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
