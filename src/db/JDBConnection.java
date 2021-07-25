6044223d24568414df454d3398c152eb957d0e76
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-07-25 12:14:41 +0530

    Correction of Javadoc

--- src/db/JDBConnection.java
+++ src/db/JDBConnection.java
@@ -2,21 +2,22 @@ package db;
 
 import java.sql.*;
 
+/**
+ * This class contains the connection to an SQL database using JDBC
+ * @author Burra Abhishek
+ */
 public class JDBConnection{
-	/**
-	 * This class contains the connectivity to an SQL database using JDBC
-	 */
 	private String serveraddr = "";
 	private String database = "";
 	private String username = "";
 	private String password = "";
 	public Connection conn = null;
+	/**
+	 * Generates a JDBC connection directly to the entire web server.
+	 * 
+	 *  @return java.sql.Connection connection, null if no connection
+	 */
 	public Connection generateServerConnection() {
-		/**
-		 * Generates a JDBC connection directly to the entire web server.
-		 * 
-		 *  @return java.sql.Connection connection
-		 */
 		try {
 			this.conn = DriverManager.getConnection(this.serveraddr, this.username, this.password);
 		} catch(Exception e) {
@@ -25,12 +26,12 @@ public class JDBConnection{
 		}
 		return this.conn;
 	}
+	/**
+	 * Generates a JDBC connection to a database in the web server.
+	 * 
+	 *  @return java.sql.Connection connection, null if no connection
+	 */
 	public Connection generateDBConnection() {
-		/**
-		 * Generates a JDBC connection to a database in the web server.
-		 * 
-		 *  @return java.sql.Connection connection
-		 */
 		try {
 			this.conn = DriverManager.getConnection(this.serveraddr + this.database, this.username, this.password);
 		} catch(Exception e) {
